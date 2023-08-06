203b969d9d6f1a9f5c1aeef61555e749cede3890
abellotest <leyvaobregonabel@gmail.com>
2023-08-05 23:51:03 -0500

    Login, Registro de usuario, Recuperar contraseña, Home, Mostrar Restaurantes, Mostrar Ruseos, Mostrar Clubes de Jazz, Añadir a favoritos, Eliminar favoritos, Subir imagen y Cerrar sesión

---
+++ src/environments/environment.ts
@@ -0,0 +1,36 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  //Credenciales Pame:
+  firebaseConfig: {
+  apiKey: "AIzaSyD-zms2CCPo1fE6SQz5mPODq5NNRvAN8pA",
+  authDomain: "appproyecto-df78f.firebaseapp.com",
+  projectId: "appproyecto-df78f",
+  storageBucket: "appproyecto-df78f.appspot.com",
+  messagingSenderId: "652747085495",
+  appId: "1:652747085495:web:4843742fd41a74c3311f32",
+  measurementId: "G-2EG4R97LV4"
+  
+    //Credenciales Domenico:
+  //  firebaseConfig : {
+  //   apiKey: "AIzaSyAQaFhilDGnUYpO5MhHZb5pQad8ARkZ8QA",
+  //   authDomain: "login-proyecto-aplicaciones.firebaseapp.com",
+  //   projectId: "login-proyecto-aplicaciones",
+  //   storageBucket: "login-proyecto-aplicaciones.appspot.com",
+  //   messagingSenderId: "82068058610",
+  //   appId: "1:82068058610:web:2f3b0ec820191915e67b6b"
+  
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
